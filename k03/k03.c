9d62c9aac048359243722f5f03d965623bd86d5d
Naoki Akiyama <bb35319003@ms.nagasaki-u.ac.jp>
2020-11-12 10:38:04 +0900

    35319003　秋山直輝

--- k03/k03.c
+++ k03/k03.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define DEBUG
 
@@ -10,13 +11,82 @@ char StrKey[] = "wind";
 char* ForceSearch(char text[], char key[])
 {
     //  ここを実装する
+    int key_len, text_len, start, pos;
+    char *result;
+    result = NULL;
+    text_len = strlen(text);
+    key_len = strlen(key);
 
+    for(start = 0; start < text_len; start++){
+        pos = 0;
+        while(pos < key_len){
+            if(text[start + pos] != key[pos]){
+                break;
+            }else{
+                result = &text[start];
+            }
+
+            pos++;
+        }
+        
+        if(pos == key_len){
+            break;
+        }
+
+    } 
+
+    return result;
 }
 
 char* BMSearch(char text[], char key[])
 {
     //  ここを実装する
+    int key_len, text_len, index, i, pos, cn;
+    char *result;
+    result = NULL;
+    int table[256];
+    key_len = strlen(key);
+    text_len = strlen(text);
+    
+    for(i = 0; i < 256; i++){
+        table[i] = 4;
+    }
+    table['w'] = 1;
+    table['i'] = 2;
+    table['n'] = 3;
+    table['d'] = 0;
+     
+    for(index = key_len - 1; index < text_len;){
+        cn = 0;
+        for(pos = key_len - 1; pos > 0; cn++){
+            if(text[index - cn] == key[pos]){
+                pos--;
+            }else if(text[index - cn] == 'w'){
+                index += table['w'];
+                break;
+            }else if(text[index - cn] == 'i'){
+                index += table['i'];
+                break;
+            }else if(text[index - cn] == 'n'){
+                index += table['n'];
+                break;
+            }else if(text[index - cn] == 'd'){
+                index += table['d'];
+                break;
+            }else{
+                index = index + table[index - cn];
+                break;
+            } 
+        }
+        
 
+        if(pos == 0){
+            result = &text[index - cn];
+            break;
+        }
+    }
+
+    return result;
 }
 
 int main(void)
